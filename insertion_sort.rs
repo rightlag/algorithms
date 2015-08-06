e7c07843aa50995aca4c0c4be92a264fdc764639
rightlag <rightlag@gmail.com>
2015-08-06 09:23:17 -0400

    Initial commit

---
+++ insertion_sort.rs
@@ -0,0 +1,17 @@
+fn insertion_sort(A: &mut Vec<i32>) -> &mut Vec<i32> {
+    for i in 1..A.len() {
+        let mut j = i;
+        let tmp = A[i];
+        while j > 0 && A[j] < A[j - 1] {
+            A[j] = A[j - 1];
+            j -= 1;
+        }
+        A[j] = tmp;
+    }
+    return A;
+}
+
+fn main() {
+    let A = &mut vec![1, 5, 2, 8];
+    println!("{:?}", insertion_sort(A));
+}
