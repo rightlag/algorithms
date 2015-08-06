02cac90bd5c7c9e0bb87f9314883686328e278b4
rightlag <rightlag@gmail.com>
2015-08-06 10:03:21 -0400

    Added base condition to check if length of vector < 2

--- insertion_sort.rs
+++ insertion_sort.rs
@@ -1,14 +1,19 @@
 fn insertion_sort(A: &mut Vec<i32>) -> &mut Vec<i32> {
-    for i in 1..A.len() {
-        let mut j = i;
-        let tmp = A[i];
-        while j > 0 && A[j] < A[j - 1] {
-            A[j] = A[j - 1];
-            j -= 1;
-        }
-        A[j] = tmp;
+    if A.len() < 2 {
+        A
     }
-    A
+    else {
+        for i in 1..A.len() {
+            let mut j = i;
+            let tmp = A[i];
+            while j > 0 && A[j] < A[j - 1] {
+                A[j] = A[j - 1];
+                j -= 1;
+            }
+            A[j] = tmp;
+        }
+        A
+    }
 }
 
 fn main() {
